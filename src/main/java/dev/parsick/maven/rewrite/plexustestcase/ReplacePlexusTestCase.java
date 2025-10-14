930a7e9dd78c69508cb659dbd354525081a1002a
Sandra Parsick <sandra@parsick.dev>
2025-10-14 18:23:40 +0200

    feat: add static imports for used PlexusExtension method

--- src/main/java/dev/parsick/maven/rewrite/plexustestcase/ReplacePlexusTestCase.java
+++ src/main/java/dev/parsick/maven/rewrite/plexustestcase/ReplacePlexusTestCase.java
@@ -3,13 +3,16 @@ package dev.parsick.maven.rewrite.plexustestcase;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.ChangeMethodTargetToStatic;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.JavaType;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 import static org.openrewrite.NlsRewrite.Description;
 import static org.openrewrite.NlsRewrite.DisplayName;
@@ -32,6 +35,19 @@ public class ReplacePlexusTestCase extends Recipe {
     }
 
     private class PlexusTestCaseVisitor extends JavaIsoVisitor<ExecutionContext> {
+
+        private static List<String> staticPlexusExtensionsMethod = new ArrayList<>() {
+            {
+                add("getTestFile");
+                add("getTestPath");
+                add("getBaseDir");
+                add("getTestConfiguration");
+            }
+        };
+
+        private static String NEW_CLASS = "org.codehaus.plexus.testing.PlexusExtension";
+        private static String OLD_CLASS = "org.codehaus.plexus.PlexusTestCase";
+
         @Override
         public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
             J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, executionContext);
@@ -43,13 +59,36 @@ public class ReplacePlexusTestCase extends Recipe {
                     .javaParser(JavaParser.fromJavaVersion()
                             .classpath("plexus-testing"))
                     .imports("org.codehaus.plexus.testing.PlexusTest")
+//                    .staticImports(staticPlexusExtensionsMethod.stream().map(name -> NEW_CLASS + "." + name).toArray(String[]::new))
                     .build()
                     .apply(getCursor(), cd.getCoordinates().addAnnotation(Comparator.comparing(J.Annotation::getSimpleName)));
             maybeAddImport("org.codehaus.plexus.testing.PlexusTest"); // both imports methods are needed to add a new import
+            staticPlexusExtensionsMethod.forEach( it -> {
+                        doAfterVisit(new ChangeMethodTargetToStatic(OLD_CLASS + " " + it + "(..)", NEW_CLASS, null, null, true).getVisitor());
+                    }
+            );
 
             cd = cd.withExtends(null);
             maybeRemoveImport("org.codehaus.plexus.PlexusTestCase");
+
             return cd;
         }
+
+
+//        @Override
+//        public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext o) {
+//            J.MethodInvocation m = super.visitMethodInvocation(method, o);
+//            if(!isPlexusTestCaseMethod(m)){
+//                return m;
+//            }
+//
+//            return m;
+//        }
+//
+//        private boolean isPlexusTestCaseMethod(J.MethodInvocation method) {
+//            return staticPlexusExtensionsMethod.contains(method.getSimpleName());
+//        }
+
+
     }
 }
--- src/test/java/dev/parsick/maven/rewrite/plexustestcase/ReplacePlexusTestCaseTest.java
+++ src/test/java/dev/parsick/maven/rewrite/plexustestcase/ReplacePlexusTestCaseTest.java
@@ -47,6 +47,47 @@ class ReplacePlexusTestCaseTest implements RewriteTest {
         ));
     }
 
+    @Test
+    void addStaticImport() {
+        rewriteRun(java(
+                """
+                        import java.io.File;
+                        import java.util.Arrays;
+                        
+                        import org.codehaus.plexus.PlexusTestCase;
+                                               
+                        public class MojoTest extends PlexusTestCase {
+                            public void testModelloConvertersMojo() throws Exception {
+                                File outputDirectory = getTestFile("target/converters-test");
+
+                                String models[] = new String[1];
+                                models[0] = getTestPath("src/test/resources/java-model.mdo");
+
+                            }
+                        }
+        """,
+                """
+                        import java.io.File;
+                        import java.util.Arrays;
+
+                        import org.codehaus.plexus.testing.PlexusTest;
+                        
+                        import static org.codehaus.plexus.testing.PlexusExtension.getTestFile;
+                        import static org.codehaus.plexus.testing.PlexusExtension.getTestPath;
+                        
+                        @PlexusTest
+                        public class MojoTest {
+                            public void testModelloConvertersMojo() throws Exception {
+                                File outputDirectory = getTestFile("target/converters-test");
+
+                                String models[] = new String[1];
+                                models[0] = getTestPath("src/test/resources/java-model.mdo");
+
+                            }
+                        }"""
+        ));
+    }
+
  
 }
 
